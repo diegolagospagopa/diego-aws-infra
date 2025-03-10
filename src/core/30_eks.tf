11fc18a8c12c08e6695dd1ad952dcd8c038057d3
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-03-10 15:39:58 +0100

    feat: add IAM role and policy for EKS

---
+++ src/core/30_eks.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "eks" {
+  name = "${local.project_name}-eks"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  role       = aws_iam_role.eks.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
