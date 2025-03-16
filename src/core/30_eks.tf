1864dffb2c4d10992ae896dff673b1aa2a27ca68
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-03-16 18:08:38 +0100

    feat: add EKS cluster resource configuration

--- src/core/30_eks.tf
+++ src/core/30_eks.tf
@@ -19,3 +19,26 @@ resource "aws_iam_role_policy_attachment" "eks" {
   role       = aws_iam_role.eks.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+resource "aws_eks_cluster" "eks" {
+  name     = "${local.project_name}-eks"
+  # version  =
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access  = true
+
+    subnet_ids = [
+      aws_subnet.private_1.id,
+      aws_subnet.private_2.id
+    ]
+  }
+
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks]
+}
