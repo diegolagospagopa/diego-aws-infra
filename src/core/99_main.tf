058e9d3211e8d7fc406cc277b164a1cf5e854014
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-03-16 18:48:42 +0100

    feat: add Kubernetes provider configuration for EKS

--- src/core/99_main.tf
+++ src/core/99_main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.87.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.36.0"
+    }
   }
 }
 
@@ -19,3 +23,14 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks.name]
+    command     = "aws"
+  }
+}
+
